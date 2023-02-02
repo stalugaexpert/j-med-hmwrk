c12f149b9498bbc860d074928eb63c486c863faa
stalugaexpert <stalegapawel@gmail.com>
2023-02-02 13:14:54 +0100

    Added schema url as a variable in codegen config file (env not supported).

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,10 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
+const apiUrl = 'https://countries.trevorblades.com'
+
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://countries.trevorblades.com',
+  schema: apiUrl,
   documents: 'src/graphql/**/*.graphql',
   generates: {
     'src/graphql/generated/': {
